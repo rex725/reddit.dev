0ded9c939f4981c4da604afe7857dcffbe385289
David R. Jex <david.jex@me.com>
2017-08-23 14:57:42 -0500

    Routes containing a function that increments by one, that adds two numbers together, and pushes a string to upper case.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/uppercase/{word}', function($word) {
+	return strtoupper($word);
+});
+
+Route::get("/increment/{number}", function($number) {
+	if (is_numeric($number)) {
+		return $number += 1;
+	} else {
+		return 1;
+	}
+});
+
+Route::get("/add/{num1}/{num2}", function($num1, $num2) {
+	return $num1 + $num2;
+});
